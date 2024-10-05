475f87d9c27d4c9e3c74b7b04c0373fffa78fc12
Arroyo75 <jszczepanski75@gmail.com>
2024-10-05 17:50:46 +0200

    Merge fixed

--- src/js/main.js
+++ src/js/main.js
@@ -3,26 +3,25 @@ import * as THREE from 'three';
 import { OrbitControls } from 'OrbitControls';
 import { solar } from './solarSystem.js';
 
-
 document.addEventListener('DOMContentLoaded', function () {
-    solar(THREE, OrbitControls); 
-});
+    const videoElement = document.querySelector('.background-clip');
+    const buttonElement = document.getElementById('button');
 
-const videoElement = document.querySelector('.background-clip');
-const buttonElement = document.getElementById('button');
-
-buttonElement.addEventListener('click', (e) => {
-    e.preventDefault(); 
-    videoElement.pause();
-    videoElement.classList.add('hidden');
-
-    setTimeout(() => {
-        document.body.classList.add('slide-up');
-        
+    buttonElement.addEventListener('click', (e) => {
 
         setTimeout(() => {
-            window.location.href = buttonElement.href; 
-        }, 500); 
-    }, 300); 
+            document.body.classList.add('slide-up');
+            
+
+            setTimeout(() => {
+                window.location.href = buttonElement.href; 
+            }, 500); 
+        }, 300); 
+        solar(THREE, OrbitControls);
+
+        e.preventDefault(); 
+        videoElement.pause();
+        videoElement.classList.add('hidden');
+      });
 });
 
