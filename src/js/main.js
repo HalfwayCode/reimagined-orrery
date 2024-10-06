91b0843f0c2461107043d1836c18c1b0b8505b32
Arroyo75 <jszczepanski75@gmail.com>
2024-10-06 14:39:56 +0200

    page transition

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const videoElement = document.querySelector('.background-clip');
     const buttonElement = document.getElementById('button');
+    const transitionElement = document.querySelector('.page-transition');
 
     if (buttonElement) {
         buttonElement.addEventListener('click', (e) => {
             e.preventDefault();
 
+            transitionElement.classList.add('active');
+
             setTimeout(() => {
-                document.body.classList.add('slide-up');
+                //document.body.classList.add('slide-up');
                 setTimeout(() => {
                     window.location.href = 'solarSystem.html';
                 }, 500);
