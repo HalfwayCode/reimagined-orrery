2e7a57edb5da48b5df7f1b0dad5b8ea3d7b94d5e
AdamSzpyt <118010833+Mamrek@users.noreply.github.com>
2024-10-06 11:30:52 +0200

    Updated distances

--- src/js/solarSystem.js
+++ src/js/solarSystem.js
@@ -2,7 +2,7 @@ import { cameraWork } from './cameraWork.js';
 
 export function solar(THREE, OrbitControls) {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
